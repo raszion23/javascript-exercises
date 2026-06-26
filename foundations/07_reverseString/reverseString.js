b1b0c01ffcccfffc685cc37b19c6682a13f43a6e
Anthony <56187194+raszion23@users.noreply.github.com>
2026-06-26 18:39:37 -0400

    Exercise completed.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    const arr = str.split('');
+    let newArr = arr.reverse();
+    return newArr.join('');
 };
+
+console.log(reverseString('Hello'));
 
 // Do not edit below this line
 module.exports = reverseString;
