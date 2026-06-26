202cd1830d90bd7565ddb83f4be2680ab6ea2005
Anthony <56187194+raszion23@users.noreply.github.com>
2026-06-26 17:38:43 -0400

    Completed exercise.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+    let returnStr = '';
 
-};
+    if (num > 0) {
+        for (let i = 0; i < num; i++) {
+            returnStr = str.concat(returnStr);
+        }
+    } else if (num < 0) {
+        returnStr = 'ERROR'
+    }
+    return returnStr;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
