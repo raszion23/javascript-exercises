d200a7fa0089047f6353903e99a3a5db1f8e9983
Anthony <56187194+raszion23@users.noreply.github.com>
2026-06-28 15:22:44 -0400

    Compeleted exercise.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...itemsToRemove) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        let currentItem = array[i];
+
+        if (!itemsToRemove.includes(currentItem)) {
+            result.push(currentItem);
+        }
+    }
+
+    return result;
+
+    // if (!Array.isArray(arr) && typeof arr !== "string") {
+    //     return 'Not an array or string';
+    // }
+
+
 };
+
+console.log(removeFromArray([1, 2, 2, 3, 4], 2, 'dog'));
 
 // Do not edit below this line
 module.exports = removeFromArray;
