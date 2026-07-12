5fab2e29fe42b8ed3e6644001731c4b71dfa45a3
Anthony <56187194+raszion23@users.noreply.github.com>
2026-07-12 13:51:58 -0400

    Exercise Completed.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let cleanedString = string.toLowerCase();
+    cleanedString = cleanedString.replace(/[^a-z0-9]/g, "");
+    return cleanedString === cleanedString.split("").reverse().join("");
 };
 
 // Do not edit below this line
